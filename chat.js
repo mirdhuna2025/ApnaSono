0e87e908991467a355e4c7e73736c519cd2f7205
mirdhuna2025 <mirdhuna2025@gmail.com>
2026-01-30 00:07:41 +0530

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,7 +1,7 @@
-/// 🔧 Fix: Remove trailing spaces in import URLs
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js"; 
-import { getDatabase, ref, push, onValue, update, remove, get } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
-import { getStorage, ref as sRef, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/storage';
+
 
 // Firebase Configuration
 const firebaseConfig = {
