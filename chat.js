f2d3f33123d226a2f613b244ed6a0f5062ad6a06
mirdhuna2025 <mirdhuna2025@gmail.com>
2026-01-30 00:15:42 +0530

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,7 +1,10 @@
-import firebase from 'firebase/app';
-import 'firebase/database';
-import 'firebase/storage';
-
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getDatabase, ref, push, onValue, remove
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+import {
+  getStorage, ref as sRef, uploadBytes, getDownloadURL
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 // Firebase Configuration
 const firebaseConfig = {
